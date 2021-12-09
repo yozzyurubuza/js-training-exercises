f3c3911e9000ee1c70fbe6331bb4d58b5abb394b
yozzyurubuza <matt_ebarle15@yahoo.com>
2021-12-09 17:06:22 +0800

    Added Exercise for Selection Sort

--- script.js
+++ script.js
@@ -44,6 +44,8 @@ compareObj(testObject1, testObject2);
 
 //Write a JavaScript program to rotate the string 'w3resource' in right direction by periodically removing one letter from the end of the string and attaching it to the front.
 
+/*
+
 const sampleString = 'w3resource';
 
 //4 different ways of transforming string to array
@@ -72,3 +74,42 @@ const invertedWordArray = rotateString(splitString);
 //Return array to string
 const invertedWordString = invertedWordArray.join('');
 console.log(invertedWordString);
+
+*/
+
+//Sorting Algorithms Practice
+
+//1. Selection Sort - find the minimum element in array and place it in front.
+
+//Arrange the unsorted array into ascending order
+
+const unSortedArr = [64, 25, 12, 22, 11];
+const unsortedArr2 = [23, 22, 51, 6, 12];
+let currentMinimum, elementToCompare, elementPosition;
+
+const selectionSort = function (array) {
+  //Position of the Array
+  for (let i = 0; i < array.length - 2; i++) {
+    //Comparing the element to the rest of the array elements
+    elementToCompare = array[i];
+    currentMinimum = elementToCompare;
+    for (let j = i + 1; j < array.length; j++) {
+      console.log(`Iteration #${j}, Current Minimum: ${currentMinimum}`);
+      if (currentMinimum > array[j]) {
+        currentMinimum = array[j];
+        elementPosition = j;
+        console.log(
+          `New Current Minimum is: ${currentMinimum}, found at Array Position ${elementPosition}`
+        );
+      } else {
+        console.log(`${currentMinimum} is lower than ${array[j]}`);
+      }
+    }
+    //Swap elements
+    array[i] = currentMinimum;
+    array[elementPosition] = elementToCompare;
+    console.log(array);
+  }
+};
+
+selectionSort(unsortedArr2);
