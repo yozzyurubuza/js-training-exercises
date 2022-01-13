5ed26e6a03263626e4a7b2ba318ee6633297848d
yozzyurubuza <matt_ebarle15@yahoo.com>
2022-01-13 19:33:50 +0800

    Added Bubble Sort in Training Exercise

--- script.js
+++ script.js
@@ -81,8 +81,19 @@ console.log(invertedWordString);
 
 //1. Selection Sort - find the minimum element in array and place it in front.
 
+/*
+Process:
+1. Get the 1st number to compare
+2. Compare the number to its right
+3. If the number is lower, set it as the current minimum then also get its array position. 
+  3.1 If not, just move on to the next number to its right.
+4. After reaching the end of the array with the 1st comparison, swap the 1st number with the recorded current minimum and array position.
+5. Repeat step 1, using the 2nd number in the array.
+*/
+
 //Arrange the unsorted array into ascending order
 
+/*
 const UNSORTED_ARRAY = [64, 25, 12, 22, 11];
 const unsortedArr2 = [23, 33, 65, 22, 7, 51, 6, 12, 4, 3];
 const unsortedArr3 = [15, 20, 25];
@@ -90,6 +101,7 @@ const unsortedArr3 = [15, 20, 25];
 let currentMinimum, elementToCompare, minimumPosition;
 
 const selectionSort = function (array) {
+  console.log(array); // Display the very first position
   //For loop i, get the element for comparison
   for (let i = 0; i < array.length - 1; i++) {
     //Comparing the element to the rest of the array elements
@@ -121,4 +133,98 @@ const selectionSort = function (array) {
   return array;
 };
 
-selectionSort(unsortedArr3);
+selectionSort(unsortedArr2);
+*/
+
+//W3schools JS Exercise - https://www.w3schools.com/js/js_exercises.asp
+
+/*
+function myFunction() {
+  alert('Hello');
+  return 'Hello';
+}
+
+// document.getElementById('dummy').innerHTML = myFunction();
+
+// const txt = 'We are "Vikings"';
+// alert(txt);
+
+//Retrieving Dates
+const d = new Date();
+// alert(d);
+const year = d.getFullYear();
+console.log(year);
+
+const month = d.getMonth();
+console.log(month);
+
+//JS Math
+
+let r = Math.random();
+console.log(r);
+
+let x = Math.max(10, 20);
+console.log(x);
+
+const fruits = ['Apple', 'Banana', 'Orange'];
+for (x in fruits) {
+  console.log(fruits[x]);
+}
+*/
+
+// Sorting Algorithm Practice
+// Bubble Sort
+
+/*
+Process:
+1. Get the 1st 2 number in the array
+2. Compare them, if the first number is higher, swap the position with second number.
+  2.1 If not, don't swap and then start comparing 2nd and 3rd number.
+3. Sorting will end if no more swapping occurs in the whole iteration.
+*/
+
+//Sort the array in ascending order using bubble sort.
+
+const UNSORTED_ARRAY = [64, 25, 12, 22, 11];
+const unsortedArr2 = [23, 33, 65, 22, 7, 51, 6, 12, 4, 3];
+const unsortedArr3 = [15, 20, 25];
+
+const bubbleSort = function (array) {
+  let repeatBubbleSort = true;
+  let firstCompare, secondCompare;
+
+  console.log(array);
+
+  while (repeatBubbleSort == true) {
+    repeatBubbleSort = false; // Initially reset repeatBubbleSort to false.
+    for (let i = 0; i < array.length - 1; i++) {
+      //First Number for Comparison
+      firstCompare = array[i];
+      secondCompare = array[i + 1];
+
+      console.log(
+        `Iteration #${i + 1}: Comparing ${firstCompare} and ${secondCompare}`
+      );
+
+      //Compare the 2 numbers, swap if first is lower
+      if (firstCompare < secondCompare) {
+        console.log(
+          `${firstCompare} is lower than ${secondCompare}, no swapping will occur.`
+        );
+        console.log(array);
+        continue;
+      }
+
+      //if first is higher
+      array[i] = secondCompare;
+      array[i + 1] = firstCompare;
+      console.log(
+        `${firstCompare} is higher than ${secondCompare}, swapping...`
+      );
+      console.log(array);
+      repeatBubbleSort = true; //Repeat bubble sort because a swapping occured.
+    }
+  }
+};
+
+bubbleSort(UNSORTED_ARRAY);
